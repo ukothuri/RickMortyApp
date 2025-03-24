3fe8526242cb27b7fa974798d859c0fcaaad6d0e
Mahesh Kothuri <101145397+ukothuri@users.noreply.github.com>
2025-03-23 20:22:03 -0700

    hilt working code with ComponentActivity

--- app/src/main/java/com/example/rickmortyapp/MainActivity.kt
+++ app/src/main/java/com/example/rickmortyapp/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.example.rickmortyapp
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.rickmortyapp.ui.CharacterFragment
+import com.example.rickmortyapp.ui.CharacterListScreen
+import com.example.rickmortyapp.ui.theme.RickMortyAppTheme
+import com.example.rickmortyapp.viewmodel.CharacterViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container_view, CharacterFragment())
-                .commit()
+        setContent {
+            RickMortyAppTheme {
+                val viewModel: CharacterViewModel = hiltViewModel()
+                CharacterListScreen(viewModel = viewModel) { character ->
+                    // handle character click
+                }
+            }
         }
     }
-
 }
